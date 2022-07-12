f81104cbb2f9e6b385909a6ce2826deef7e2a986
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 09:01:03 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio15
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Escriba un número:");
+            double numero = Convert.ToDouble(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número SÍ es divisible entre 2.");
+            }
+            else
+            {
+                Console.WriteLine("El número NO es divisible entre 2");
+            }
+        }
+    }
+}
